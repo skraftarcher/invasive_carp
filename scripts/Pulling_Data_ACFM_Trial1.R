7e64fb6e5bb88fb67b2fd5798568187b60dd3484
abigailhost <abigailhost7@gmail.com>
2022-07-08 13:39:12 -0500

    updated script for pulling data for samplingcalculations.R and stockoutcalculations.R from ACFM Feeding Trial 1

---
+++ scripts/Pulling_Data_ACFM_Trial1.R
@@ -0,0 +1,12 @@
+# pulling/downloading data sheets from ACFM Feeding Trial 1
+
+
+#downloading gsheet package
+if(!require(gsheet))install.packages("gsheet");library(gsheet)
+
+
+# pull the samplingcalculations.R data from ACFM Tial 1 google drive
+
+wqurl <- "https://docs.google.com/spreadsheets/d/1pPBfcCelEU_XjhyCIuJY7qCNYh-ufLbA/edit#gid=1011687095"
+
+wqurl <- "https://docs.google.com/spreadsheets/d/1pPBfcCelEU_XjhyCIuJY7qCNYh-ufLbA/edit#gid=1549175690"

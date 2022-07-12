7b7ca99a4a7b64bf191bd29480ea220f58880a48
abigailhost <abigailhost7@gmail.com>
2022-07-12 16:03:11 -0500

    sampling data- establishing reference points

--- scripts/Pulling_Data_ACFM_Trial1.R
+++ scripts/Pulling_Data_ACFM_Trial1.R
@@ -3,11 +3,11 @@
 
 #downloading gsheet package
 if(!require(gsheet))install.packages("gsheet");library(gsheet)
-if(!require(car)){install.packages("car")}
-if(!require(lsmeans)){install.packages("lsmeans")}
-if(!require(multcomp)){install.packages("multcomp")}
-if(!require(lmerTest)){install.packages("lmerTest")}
-
+if(!require(car)){install.packages("car")};library(car)
+if(!require(lsmeans)){install.packages("lsmeans")};library(lsmeans)
+if(!require(multcomp)){install.packages("multcomp")};library(multcomp)
+if(!require(lmerTest)){install.packages("lmerTest")};library(lmerTest)
+if(!require(tidyverse)){install.packages("tidyverse")};library(tidyverse)
 # pull the samplingcalculations.R data from ACFM Tial 1 google drive
 
 saurl <- "https://docs.google.com/spreadsheets/d/1pPBfcCelEU_XjhyCIuJY7qCNYh-ufLbA/edit#gid=1011687095"
@@ -18,7 +18,15 @@ samp <- read.csv(text=gsheet2text(saurl,format="csv"),stringsAsFactors = F)
 
 stock <- read.csv(text=gsheet2text(sturl,format="csv"),stringsAsFactors = F)
 
+#organize data a  bit
+samp$diet <- factor(samp$diet,levels=c(1,2,3))
+
 #making linear model for HSI in samplingcalculations
 
-HSI.lm <- lm(HSI~diet.name,samp)
+HSI.lm <- lmer(HSI~diet+(1|tank),samp)
 plot(HSI.lm)
+summary(HSI.lm)
+#example of how to re-level
+HSI.lm.r2 <- lmer(HSI~diet+(1|tank),samp%>%
+                    mutate(diet=relevel(diet,ref="2")))
+summary(HSI.lm.r2)

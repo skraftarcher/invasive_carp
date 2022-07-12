6e1029cc0e036fa0414904cec913640719e90070
abigailhost <abigailhost7@gmail.com>
2022-07-12 15:40:09 -0500

    start analysis for catfish

--- scripts/Pulling_Data_ACFM_Trial1.R
+++ scripts/Pulling_Data_ACFM_Trial1.R
@@ -3,10 +3,22 @@
 
 #downloading gsheet package
 if(!require(gsheet))install.packages("gsheet");library(gsheet)
-
+if(!require(car)){install.packages("car")}
+if(!require(lsmeans)){install.packages("lsmeans")}
+if(!require(multcomp)){install.packages("multcomp")}
+if(!require(lmerTest)){install.packages("lmerTest")}
 
 # pull the samplingcalculations.R data from ACFM Tial 1 google drive
 
-wqurl <- "https://docs.google.com/spreadsheets/d/1pPBfcCelEU_XjhyCIuJY7qCNYh-ufLbA/edit#gid=1011687095"
+saurl <- "https://docs.google.com/spreadsheets/d/1pPBfcCelEU_XjhyCIuJY7qCNYh-ufLbA/edit#gid=1011687095"
 
-wqurl <- "https://docs.google.com/spreadsheets/d/1pPBfcCelEU_XjhyCIuJY7qCNYh-ufLbA/edit#gid=1549175690"
+sturl <- "https://docs.google.com/spreadsheets/d/1pPBfcCelEU_XjhyCIuJY7qCNYh-ufLbA/edit#gid=1549175690"
+
+samp <- read.csv(text=gsheet2text(saurl,format="csv"),stringsAsFactors = F)
+
+stock <- read.csv(text=gsheet2text(sturl,format="csv"),stringsAsFactors = F)
+
+#making linear model for HSI in samplingcalculations
+
+HSI.lm <- lm(HSI~diet.name,samp)
+plot(HSI.lm)

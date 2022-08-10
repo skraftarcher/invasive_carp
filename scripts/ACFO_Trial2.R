377f7b064724f0024bca167156855f211729c23b
abigailhost <abigailhost7@gmail.com>
2022-08-10 09:17:19 -0500

    completion of HSI, VSI, MR, and condition factor calculations

--- scripts/ACFO_Trial2.R
+++ scripts/ACFO_Trial2.R
@@ -44,3 +44,64 @@ ggplot(data=HSI.2.emm.df,aes(x=diet.name,y=emmean))+
   xlab("")+
   ylab("HSI")+
   geom_errorbar(aes(ymin=lower.CL,ymax=upper.CL),width=.1)
+
+#making linear model for VSI in samplingcalculations.2.r
+
+VSI.2.lm <- lmer(VSI~diet.name+(1|tank),samp2)
+plot(VSI.2.lm)
+summary(VSI.2.lm)
+anova(VSI.2.lm)
+#contrasts for VSI
+(VSI.2.emm <- emmeans(VSI.2.lm, "diet.name"))
+VSI.2.emm.df <- data.frame(VSI.2.emm)
+contrast(VSI.2.emm, conts,adjust="sidak")
+
+#visualizing VSI contrast results
+ggplot(data=VSI.2.emm.df,aes(x=diet.name,y=emmean))+
+  geom_point()+
+  xlab("")+
+  ylab("VSI")+
+  geom_errorbar(aes(ymin=lower.CL,ymax=upper.CL),width=.1)
+
+
+#making linear model for MR in samplingcalculations.2.r
+
+MR.2.lm <- lmer(MR~diet.name+(1|tank),samp2)
+plot(MR.2.lm)
+summary(MR.2.lm)
+anova(MR.2.lm)
+#contrasts for MR
+(MR.2.emm <- emmeans(MR.2.lm, "diet.name"))
+MR.2.emm.df <- data.frame(MR.2.emm)
+contrast(MR.2.emm, conts,adjust="sidak")
+
+#visualizing MR contrast results
+ggplot(data=MR.2.emm.df,aes(x=diet.name,y=emmean,color=diet.name,fill=diet.name))+
+  geom_errorbar(aes(ymin=lower.CL,ymax=upper.CL),width=.1,color="black")+geom_point()+
+  xlab("")+
+  ylab("Muscle Ratio")+
+  theme(legend.position = "none",
+        axis.text = element_text(size=12),
+        axis.title.y = element_text(size=16))
+
+
+#making linear model for cond.factor in samplingcalculations.2.r
+
+cf.2.lm <-lmer(cond.factor~diet.name+(1|tank),samp2)
+plot(cf.2.lm)
+summary(cf.2.lm)
+anova(cf.2.lm)
+#contrasts for condition factor
+(cf.2.emm<-emmeans(cf.2.lm,"diet.name"))
+cf.2.emm.df<-data.frame(cf.2.emm)
+contrast(cf.2.emm, conts,adjust="sidak")
+
+#visualizing condition factor contrast results
+ggplot(data=cf.2.emm.df,aes(x=diet.name,y=emmean))+
+  geom_errorbar(aes(ymin=lower.CL,ymax=upper.CL),width=.1)+
+  geom_point()+
+  xlab("")+
+  ylab("Condition Factor")+
+  theme(legend.position = "none",
+        axis.text = element_text(size=12),
+        axis.title.y = element_text(size=16))

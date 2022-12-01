ec200151111e9e5c2353b2abd4d480c938b08f9e
abigailhost <abigailhost7@gmail.com>
2022-12-01 08:48:27 -0600

    script for joining data for UM submission directory

---
+++ scripts/sample_submission_UM_1022.R
@@ -0,0 +1,11 @@
+#10/2022 Sample Submission to U of Missouri
+
+#joiining together two excel files: UM Submission directory and Moisture Content Data
+library(dplyr)
+UM <- read.csv("um_prox_data.csv")
+moisture <- read.csv("moisture_content.csv")
+inner_join(x=UM, y=moisture, by="Sample")
+sampledirectory <- inner_join(x=UM, y=moisture, by="Sample")
+
+#exporting table
+write.table(sampledirectory,file="UM_Sample_Submission_1022.csv", sep = ",")
